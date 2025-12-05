36966a8fa7a937788a67a2b0801b03ca0c294382
gergo <kiss.gergely@gmail.com>
2025-12-05 21:59:47 +0100

    added touch detection

--- src/main.py
+++ src/main.py
@@ -1,20 +1,38 @@
 from lib.LCD_1inch28 import LCD_1inch28
+from lib.Touch_CST816T import Touch_CST816T
 import time
 
 
 LCD = LCD_1inch28()
 LCD.set_bl_pwm(65535 * 60 // 100)
 
-def show_frame(frame_name, delay_ms=1000):
-    with open(f"/resources/{frame_name}.rgb565", "rb") as f:
-        f.readinto(LCD.buffer)
-    LCD.show()
-    time.sleep_ms(delay_ms)
+Touch=Touch_CST816T(mode=1,LCD=LCD)
+
+Touch.Mode = 0
+Touch.Set_Mode(Touch.Mode)
+
+LCD.fill(LCD.white)
+LCD.write_text('Gesture test',70,90,1,LCD.black)
+LCD.show()
 
 while True:
-    show_frame('hamster_240_default')
-    show_frame('hamster_240_content')
-    show_frame('hamster_240_happy')
-    show_frame('hamster_240_scared')
-    show_frame('hamster_240_sick')
-    show_frame('hamster_240_dead')
+    if Touch.Gestures > 0:
+        LCD.fill(LCD.white)
+        
+        if Touch.Gestures == 0x01:
+            LCD.write_text('UP',100,110,3,LCD.black)
+        elif Touch.Gestures == 0x02:
+            LCD.write_text('DOWN',70,110,3,LCD.black)
+        elif Touch.Gestures == 0x03:
+            LCD.write_text('LEFT',70,110,3,LCD.black)
+        elif Touch.Gestures == 0x04:
+            LCD.write_text('RIGHT',70,110,3,LCD.black)
+        elif Touch.Gestures == 0x0C:
+            LCD.write_text('Long Press',40,110,2,LCD.black)
+        elif Touch.Gestures == 0x0B:
+            LCD.write_text('Double Click',25,110,2,LCD.black)
+        
+        LCD.show()
+        time.sleep(1.0)
+    else:
+        time.sleep(0.1)
