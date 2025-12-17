867c027e316d376cbec04feb6496f720a8782be1
gergo <kiss.gergely@gmail.com>
2025-12-17 09:01:22 +0100

    made hamster time-agnostic

--- src/hamster.py
+++ src/hamster.py
@@ -1,44 +1,94 @@
 from lib.common import Battery, Gyro, LCD, Touch, Piezo
 import math
-import time
 
 
-TICK_MS = 10
+class Hamster:
+    def __init__(self, lcd: LCD, touch: Touch, gyro: Gyro, piezo: Piezo, battery: Battery, now_ms: int, tick_ms: int):
+        self.face = Face(lcd, battery)
+        self.touch = touch
+        self.gyro = gyro
+        self.player = PiezoPlayer(piezo, tick_ms)
 
-class HamsterSimulator:
-    def __init__(self, lcd: LCD, touch: Touch, gyro: Gyro, piezo: Piezo, battery: Battery):
-        self.hamster = Hamster(lcd, touch, gyro, piezo, battery)
+        self.freefall_started_at = 0
+
+        # Initial state
+        lcd.set_brightness(60)
+        self.player.play(now_ms, SOUND_HELLO)
     
-    def loop(self):
-        last = time.ticks_ms()
-        while True:
-            now = time.ticks_ms()
-            dt_ms = time.ticks_diff(now, last)
-            
-            self.hamster.simulate(dt_ms)
-            self.hamster.player.next_tune(now)
-            self.hamster.face.draw_face(now)
-            self.hamster.face.draw_battery_status()
-            self.hamster.face.show()
-            
-            last = now
-            time.sleep_ms(TICK_MS)
+    def tick(self, now_ms: int):
+        self._simulate(now_ms)
+        self.player.next_tune(now_ms)
+        self.face.draw_face(now_ms)
+        self.face.draw_battery_status()
+        self.face.show()
+        self.last_tick_ms = now_ms
+    
+    def _simulate(self, now_ms: int):
+        
+        # Check gestures
+        gesture = self.touch.gesture
+        if gesture == Touch.UP: # Up -> feed
+            self.face.set_next_face("eating", now_ms, 1000)
+        elif gesture == Touch.DOWN: # Down -> pet
+            self.face.set_next_face("cotnent", now_ms, 1000)
+        
+        # Check gyro
+        ax, ay, az, gx, gy, gz = self.gyro.read_axyz_gxyz()
+        if self.detect_drop(ax, ay, az, now_ms): # Dropped -> dead
+            self.face.set_next_face("dead", now_ms, 60000, override=True)
+        elif self.detect_shake(ax, ay, az, gx, gy, gz): # Shaken -> scared
+            self.player.play(now_ms, SOUND_EEEK)
+            self.face.set_next_face("scared", now_ms, 2000, override=True)
+        elif self.detect_gentle(ax, ay, az, gx, gy, gz): # Gently moved -> happy
+            self.face.set_next_face("happy", now_ms, 2000)
+    
+    def detect_drop(self, ax: float, ay: float, az: float, now_ms: int) -> bool:
+        mag2 = ax*ax + ay*ay + az*az
+        if mag2 >= 0.25: # >= (0.5g)^2 -> not freefall
+            self.freefall_started_at = 0
+            return False
+        elif not self.freefall_started_at: # Freefall just started
+            self.freefall_started_at = now_ms
+            return False
+        elif now_ms - self.freefall_started_at < 30: # Freefalling but not enough yet
+            return False
+        else:
+            self.freefall_started_at = 0 # Hamster's doomed
+            return True
+
+    BASELINE_A = 1.018      # 1.018 is a baseline computed while still, could determine it dynamically
+    SHAKE_ACCEL_DEV = 0.15  # deviation from gravity (g)
+    SHAKE_GYRO = 240        # deg/s
+    GENTLE_DEV_MIN = 0.07    # just above noise
+
+    def detect_shake(self, ax: float, ay: float, az: float, gx: float, gy: float, gz: float) -> bool:
+        a = math.sqrt(ax*ax + ay*ay + az*az)
+        dev = abs(a - Hamster.BASELINE_A)
+        g = max(abs(gx), abs(gy), abs(gz))
+        return dev > Hamster.SHAKE_ACCEL_DEV or g > Hamster.SHAKE_GYRO
+
+    def detect_gentle(self, ax: float, ay: float, az: float, gx: float, gy: float, gz: float) -> bool:
+        a = math.sqrt(ax*ax + ay*ay + az*az)
+        dev = abs(a - Hamster.BASELINE_A)
+        g = max(abs(gx), abs(gy), abs(gz))
+        return Hamster.GENTLE_DEV_MIN < dev < Hamster.SHAKE_ACCEL_DEV and g < Hamster.SHAKE_GYRO
 
 class PiezoPlayer:
     _BASE_FREQ = 8000 # Hz
 
-    def __init__(self, piezo: Piezo):
+    def __init__(self, piezo: Piezo, tick_ms: int):
         self.piezo = piezo
+        self.tick_ms = tick_ms
         self.start_time = 0
 
-    def play(self, tune: bytes):
+    def play(self, now_ms: int, tune: bytes):
         """Sets the tune in the following format:
         freq_div [0-255]
         volume [0-100]
         at_tick [0-255]
         """
         self.tune = tune
-        self.start_time = time.ticks_ms()
+        self.start_time = now_ms
         self.next_time = 0
         self.next_idx = 0
 
@@ -52,7 +102,7 @@ class PiezoPlayer:
 
             if current_time >= self.next_time:
                 self.piezo.beep(PiezoPlayer._BASE_FREQ // freq_div, volume)
-                self.next_time = self.start_time + (at_tick * TICK_MS)
+                self.next_time = self.start_time + (at_tick * self.tick_ms)
                 self.next_idx = i
                 return
         self.piezo.beep(0, 0)
@@ -79,22 +129,21 @@ class Face:
         self.reset_at = 0
         self.face_id = "default"
     
-    def set_next_face(self, face_id: str, duration_ms, override=False, delay_ms = 0):
-        now = time.ticks_ms()
-        if not override and now < self.reset_at:
+    def set_next_face(self, face_id: str, now_ms: int, duration_ms: int, override=False, delay_ms = 0):
+        if not override and now_ms < self.reset_at:
             return # Previous face still showing
         
-        self.start_at = now + delay_ms
-        self.reset_at = now + delay_ms + duration_ms
+        self.start_at = now_ms + delay_ms
+        self.reset_at = now_ms + delay_ms + duration_ms
         self.face_id = face_id
 
-    def draw_face(self, current_time):
+    def draw_face(self, now_ms):
         if not self.face_id:
             return
-        if current_time >= self.reset_at:
+        if now_ms >= self.reset_at:
             self._load_face("default")
             self.face_id = None
-        elif current_time >= self.start_at:
+        elif now_ms >= self.start_at:
             self._load_face(self.face_id)
     
     def draw_battery_status(self):
@@ -110,63 +159,3 @@ class Face:
     def _load_face(self, face_id: str):
         with open(f"/resources/hamster_240_{face_id}.rgb565", "rb") as f:
             f.readinto(self.lcd.buffer)
-
-class Hamster:
-    def __init__(self, lcd: LCD, touch: Touch, gyro: Gyro, piezo: Piezo, battery: Battery):
-        self.face = Face(lcd, battery)
-        self.touch = touch
-        self.gyro = gyro
-        self.player = PiezoPlayer(piezo)
-
-        self.freefall_ms = 0
-
-        # Initial state
-        lcd.set_brightness(60)
-        self.player.play(SOUND_HELLO)
-    
-    def simulate(self, dt_ms: int):
-        
-        # Check gestures
-        gesture = self.touch.gesture
-        if gesture == Touch.UP: # Up -> feed
-            self.face.set_next_face("eating", 1000)
-        elif gesture == Touch.DOWN: # Down -> pet
-            self.face.set_next_face("cotnent", 1000)
-        
-        # Check gyro
-        ax, ay, az, gx, gy, gz = self.gyro.read_axyz_gxyz()
-        if self.detect_drop(ax, ay, az, dt_ms): # Dropped -> dead
-            self.face.set_next_face("dead", 60000, override=True)
-        elif self.detect_shake(ax, ay, az, gx, gy, gz): # Shaken -> scared
-            self.player.play(SOUND_EEEK)
-            self.face.set_next_face("scared", 2000, override=True)
-        elif self.detect_gentle(ax, ay, az, gx, gy, gz): # Gently moved -> happy
-            self.face.set_next_face("happy", 2000)
-    
-    def detect_drop(self, ax: float, ay: float, az: float, dt_ms: int) -> bool:
-        a = math.sqrt(ax*ax + ay*ay + az*az)
-        if a < 0.5: # near zero-g
-            self.freefall_ms += dt_ms
-            if self.freefall_ms > 30: # for more than 30 ms
-                self.freefall_ms = 0
-                return True
-        else:
-            self.freefall_ms = 0
-        return False
-
-    BASELINE_A = 1.018      # 1.018 is a baseline computed while still, could determine it dynamically
-    SHAKE_ACCEL_DEV = 0.15  # deviation from gravity (g)
-    SHAKE_GYRO = 240        # deg/s
-    GENTLE_DEV_MIN = 0.07    # just above noise
-
-    def detect_shake(self, ax: float, ay: float, az: float, gx: float, gy: float, gz: float) -> bool:
-        a = math.sqrt(ax*ax + ay*ay + az*az)
-        dev = abs(a - Hamster.BASELINE_A)
-        g = max(abs(gx), abs(gy), abs(gz))
-        return dev > Hamster.SHAKE_ACCEL_DEV or g > Hamster.SHAKE_GYRO
-
-    def detect_gentle(self, ax: float, ay: float, az: float, gx: float, gy: float, gz: float) -> bool:
-        a = math.sqrt(ax*ax + ay*ay + az*az)
-        dev = abs(a - Hamster.BASELINE_A)
-        g = max(abs(gx), abs(gy), abs(gz))
-        return Hamster.GENTLE_DEV_MIN < dev < Hamster.SHAKE_ACCEL_DEV and g < Hamster.SHAKE_GYRO
--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
-from hamster import HamsterSimulator
+from hamster import Hamster
 from lib.waveshare import Battery_WS_RP2350, Gyro_QMI8658, LCD_GC9A01A, Piezo_WS_RP2350, Touch_CST816T
 import sys
+import time
 
+
+TICK_MS = 10
 
 try:
-    HamsterSimulator(
+    hs = Hamster(
         LCD_GC9A01A(), 
         Touch_CST816T(),
         Gyro_QMI8658(),
         Piezo_WS_RP2350(),
-        Battery_WS_RP2350()
-    ).loop()
+        Battery_WS_RP2350(),
+        time.ticks_ms(),
+        TICK_MS
+    )
+    while True:
+        hs.tick(time.ticks_ms())
+        time.sleep_ms(TICK_MS)
 except Exception as e:
     sys.print_exception(e)
